71fbfc0ca3dacae263e2ce69cbb2a0dc901ccabe
HeliIsMe <helaineduplessis@thisisme.com>
2026-01-07 10:55:12 +0200

    upload to s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,14 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Deploying to AWS S3');
+   const bucket = core.getInput('bucket',{ required:true})
+   const bucketRegion = core.getInput('bucket-region',{ required:true})
+   const distFolder = core.getInput('dist-folder',{ required:true})
+
+   // Upload files
+   const s3Uri = `s3://${bucket}`
+   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} -- region ${bucketRegion}`)
+   core.notice('Deploying to AWS S3');
 }
 
 run();
